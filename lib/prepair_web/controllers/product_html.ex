80f21127f4d2d60cbfac36ca2d0ad0a93d178f61
Guillaume Cugnet <guillaume@cugnet.eu>
2023-06-29 21:51:23 +0200

    feat(product): add input field to select a category

--- lib/prepair_web/controllers/product_html.ex
+++ lib/prepair_web/controllers/product_html.ex
@@ -10,4 +10,14 @@ defmodule PrepairWeb.ProductHTML do
   attr :action, :string, required: true
 
   def product_form(assigns)
+
+  defp category_opts(changeset) do
+    existing_ids =
+      changeset
+      |> Ecto.Changeset.get_change(:categories, [])
+      |> Enum.map(& &1.data.id)
+
+    for cat <- Prepair.Data.list_categories(),
+        do: [key: cat.name, value: cat.id, selected: cat.id in existing_ids]
+  end
 end
